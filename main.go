962d012ed07cd95b069da5f6d5d62215ac8ffffb
Murat Shagirov <muratshagirov@gmail.com>
2025-07-19 23:42:27 +0800

    server

--- main.go
+++ main.go
@@ -1,5 +1,17 @@
 package main
 
-func main() {
+import (
+	"log"
+	"net/http"
+)
 
+func main() {
+	const port = "8080"
+	mux := http.NewServeMux()
+	s := &http.Server{
+		Handler: mux,
+		Addr:    ":" + port,
+	}
+	log.Printf("Serving on port: %s\n", port)
+	log.Fatal(s.ListenAndServe())
 }
