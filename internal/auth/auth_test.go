ced6e43e3271696499699a9314f0488b888f2955
Murat Shagirov <muratshagirov@gmail.com>
2025-07-31 11:06:39 +0800

    auth funcs

---
+++ internal/auth/auth_test.go
@@ -0,0 +1,13 @@
+package auth
+
+import (
+	"testing"
+)
+
+func TestHashPassword(t *testing.T) {
+	// HashPassword(password string) (string, error)
+}
+
+func TestCheckPasswordHash(t *testing.T) {
+	// CheckPasswordHash(password, hash string) error
+}
