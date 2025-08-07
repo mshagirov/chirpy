2f49e6f23b70b4b24f4560c48574854db41a5c70
Murat Shagirov <muratshagirov@gmail.com>
2025-08-08 04:50:55 +0800

    housekeeping

--- api_chirps.go
+++ api_chirps.go
@@ -57,41 +57,6 @@ func (cfg *apiConfig) serveCreateChirp(w http.ResponseWriter, r *http.Request) {
 	}, http.StatusCreated, w, r)
 }
 
-func (cfg *apiConfig) serveGetChirps(w http.ResponseWriter, r *http.Request) {
-	var user_id uuid.UUID
-	var chirps []database.Chirp
-	var err error
-	isForAuthor := false
-	// Query: api/chirps?author_id=AUTHORS
-	if authorIDStr := r.URL.Query().Get("author_id"); len(authorIDStr) > 0 {
-		user_id, err = uuid.Parse(authorIDStr)
-		if err == nil {
-			isForAuthor = true
-		}
-	}
-	if isForAuthor {
-		chirps, err = cfg.db.GetChirpsWithUserID(r.Context(), user_id)
-	} else {
-		chirps, err = cfg.db.GetChirps(r.Context())
-	}
-	if err != nil {
-		errorResponse(fmt.Sprintf("Database error: %s", err), http.StatusServiceUnavailable, w, r)
-		return
-	}
-	var returnVals []Chirp
-	for _, c := range chirps {
-		returnVals = append(returnVals,
-			Chirp{
-				ID:        c.ID,
-				CreatedAt: c.CreatedAt,
-				UpdatedAt: c.UpdatedAt,
-				Body:      c.Body,
-				UserID:    c.UserID,
-			})
-	}
-	jsonResponse(returnVals, http.StatusOK, w, r)
-}
-
 func (cfg *apiConfig) serveGetChirpWithID(w http.ResponseWriter, r *http.Request) {
 	strID := r.PathValue("chirpID")
 	chirpID, err := uuid.Parse(strID)
@@ -138,4 +103,47 @@ func (cfg *apiConfig) serveDeleteChirpWithID(w http.ResponseWriter, r *http.Requ
 		errorResponse(fmt.Sprintf("Database error: %v", err), http.StatusBadRequest, w, r)
 	}
 	w.WriteHeader(http.StatusNoContent)
+}
+
+func (cfg *apiConfig) serveGetChirps(w http.ResponseWriter, r *http.Request) {
+	chirps, err := getChirpsWithQuery(cfg, w, r)
+	if err != nil {
+		return
+	}
+	var returnVals []Chirp
+	for _, c := range chirps {
+		returnVals = append(returnVals,
+			Chirp{
+				ID:        c.ID,
+				CreatedAt: c.CreatedAt,
+				UpdatedAt: c.UpdatedAt,
+				Body:      c.Body,
+				UserID:    c.UserID,
+			})
+	}
+	jsonResponse(returnVals, http.StatusOK, w, r)
+}
+
+func getChirpsWithQuery(cfg *apiConfig, w http.ResponseWriter, r *http.Request) ([]database.Chirp, error) {
+	var user_id uuid.UUID
+	var chirps []database.Chirp
+	var err error
+	isForAuthor := false
+	// Query: api/chirps?author_id=AUTHORS
+	if authorIDStr := r.URL.Query().Get("author_id"); len(authorIDStr) > 0 {
+		user_id, err = uuid.Parse(authorIDStr)
+		if err == nil {
+			isForAuthor = true
+		}
+	}
+	if isForAuthor {
+		chirps, err = cfg.db.GetChirpsWithUserID(r.Context(), user_id)
+	} else {
+		chirps, err = cfg.db.GetChirps(r.Context())
+	}
+	if err != nil {
+		errorResponse(fmt.Sprintf("Database error: %s", err), http.StatusServiceUnavailable, w, r)
+		return chirps, err
+	}
+	return chirps, nil
 }
